34ada9459e9231fd249b274eff18958ea2f3432c
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2021-05-25 21:38:29 +0300

    wip-admin-chat

--- my-chat/src/App.js
+++ my-chat/src/App.js
@@ -1,2 +1,17 @@
-import  { ChatEngine} from 'react-chat-engine';
+import React from 'react';
+import {ChatEngine} from 'react-chat-engine';
+
 import './App.css';
+
+const App = () => {
+    return(
+        <ChatEngine
+        //we passing different props here to experiment our self how they behave
+            height="100vh"
+            projectID="b99c883d-7eb0-4032-bd68-cd964d59ea67"
+            userName="maria"
+            userSecret="123123"
+        />
+    )
+}
+export default App;
